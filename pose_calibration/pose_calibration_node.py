70692bf47033d53ada66f9750eb9b91a1e96ce64
hanifmb <hanifdiyer@gmail.com>
2023-08-21 06:38:25 +0200

    able to indentify planes, verbose

--- pose_calibration/pose_calibration_node.py
+++ pose_calibration/pose_calibration_node.py
@@ -3,162 +3,298 @@ import pandas as pd
 import sensor_msgs_py.point_cloud2 as pc
 import itertools
 import numpy as np 
+import struct
+import ctypes
+import cv2
+import time
+import random
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2
 from plyfile import PlyData, PlyElement
 from scipy.spatial.transform import Rotation
+from geometry_msgs.msg import TransformStamped
+from tf2_msgs.msg import TFMessage
+
+class Plane():
+  def __init__(self, three_points):
+    self.three_points = three_points
+    self.a, self.b, self.c, self.d = self.fitPlane(three_points)
+    self.inlier_indices = []
+    self.normal = [self.a, self.b, self.c]
+    self.coeff = [self.a, self.b, self.c, self.d]
+
+  def fitPlane(self, three_points):
+    v1 = three_points[1] - three_points[0]
+    v2 = three_points[2] - three_points[0]
+    normal = np.cross(v1, v2)
+    normal /= np.linalg.norm(normal)
+    d = -np.dot(normal, three_points[0])
+    return normal[0], normal[1], normal[2], d
+
+class Cloud():
+  # helper class to deal with ros PointCloud2
+  def __init__(self, cloud):
+    self.points, self.rgb = self.unpack_to_numpy(cloud)
+
+  def unpack_to_numpy(self, cloud):
+    SIZE = len(cloud)
+    x=[]; y=[]; z=[]; r=[]; g=[]; b=[]
+    for idx in range(SIZE):
+      if cloud[idx][2] <= 1.5:
+        x.append(cloud[idx][0])
+        y.append(cloud[idx][1])
+        z.append(cloud[idx][2])
+
+        test = cloud[idx][3]
+        # cast float32 to int so that bitwise operations are possible
+        s = struct.pack('>f' ,test)
+        i = struct.unpack('>l',s)[0]
+
+        pack = ctypes.c_uint32(i).value
+        rr = (pack & 0x00FF0000)>> 16
+        gg = (pack & 0x0000FF00)>> 8
+        bb = (pack & 0x000000FF)
+
+        r.append(rr)
+        g.append(gg)
+        b.append(bb)
+
+    points = np.column_stack((x, y, z))
+    rgb = np.column_stack((r, g, b))
+    return points, rgb
+
+def seq_RANSAC(cloud, threshold=0.005, n_planes=3):
+  def ransac_plane_fit(points, n_iters):
+    best_plane = []
+    best_inliers = []
+    max_inliers = 0
+
+    for i in range(n_iters):
+      random_indices = np.random.choice(len(points), 3, replace=False) # select three points randomly
+      sample_points = points[random_indices]
+      plane = Plane(sample_points) # fit a plane 
+      distances = np.abs(np.dot(points - sample_points[0], plane.normal)) # calc distance
+      inliers = np.where(distances < threshold)[0]
+      num_inliers = len(inliers) # calc num of inliers
+      # Update the best plane if necessary
+      if num_inliers > max_inliers:
+        best_plane = plane
+        best_plane.inlier_indices = inliers
+        max_inliers = num_inliers
+    return best_plane
+
+  n_points = len(cloud.points)
+  points_indices = np.arange(n_points)
+  remaining_points = cloud.points
+
+  final_planes = []
+  final_inliers = []
+  for i in range(n_planes):
+    # roughly calculate num of iterations (N)
+    p = 0.99; s = 3; num_inliers = 45000
+    eps = (len(remaining_points) - num_inliers) / len(remaining_points)
+    N = round(np.log(1-p) / np.log(1-(1-eps)**s)) 
+    N = 1000 if N > 1000 else N
+    plane = ransac_plane_fit(remaining_points, N) # run ransac
+    # update the inlier to the global indices after removing
+    local_points_indices = plane.inlier_indices.copy()
+    plane.inlier_indices[:] = points_indices[local_points_indices]
+    final_planes.append(plane)
+    # remove the inliers
+    mask = np.ones(len(remaining_points), dtype=bool)
+    mask[local_points_indices] = False
+    remaining_points = remaining_points[mask]
+    # remove the original points indices belong to inliers
+    mask2 = np.ones(len(points_indices), dtype=bool)
+    mask2[local_points_indices] = False
+    points_indices = points_indices[mask2]
+  return final_planes
+
+def gram_schmidt(vectors):
+  num_vectors, vector_length = vectors.shape
+  orthogonalized_vectors = np.zeros((num_vectors, vector_length))
+
+  for i in range(num_vectors):
+    vector = vectors[i]
+    for j in range(i):
+      projection = np.dot(vectors[i], orthogonalized_vectors[j]) / np.dot(orthogonalized_vectors[j], orthogonalized_vectors[j])
+      vector -= projection * orthogonalized_vectors[j]
+    orthogonalized_vectors[i] = vector / np.linalg.norm(vector)
+
+  return orthogonalized_vectors
+
+def vec_from_planes_intersection(plane1, plane2):
+  # normal1 = plane1.normal
+  # normal2 = plane2.normal
+
+  dir_vec = np.cross(plane1.normal, plane2.normal)
+  dir_vec_normal = dir_vec / np.linalg.norm(dir_vec)
+
+  z = dir_vec_normal[2]
+
+  # make sure the three vectors are facing the camera
+  if z > 0:
+    dir_vec_normal = dir_vec_normal * -1
+
+  return dir_vec_normal
+
+def export_planes(points, planes):
+  length = len(points)
+  rgb = np.full((length, 3), (255, 255, 255)) 
+
+  rgb[planes[0].inlier_indices] = (255, 0, 0)
+  rgb[planes[1].inlier_indices] = (0, 255, 0)
+  rgb[planes[2].inlier_indices] = (0, 0, 255)
+
+  rgb_np = np.array(rgb)
+  ver = np.hstack((points, rgb_np))
+  new_ver = np.core.records.fromarrays(ver.transpose(), 
+                                       names='x, y, z, red, green, blue',
+                                       formats = 'f4, f4, f4, u1, u1, u1')
+  el = PlyElement.describe(new_ver, 'vertex')
+  PlyData([el], text=True).write('ascii.ply')
 
 class MyNode(Node):
-    def __init__(self):
-        super().__init__('my_node')
-        self.pc_sub = self.create_subscription(PointCloud2, '/camera/depth/color/points', self.pc_cb, 10)
+  def __init__(self):
+    super().__init__('my_node')
+    self.pc_sub = self.create_subscription(PointCloud2, '/camera/depth/color/points', self.pc_cb, 10)
+    self.publisher_ = self.create_publisher(TFMessage, '/tf', 10)  # Change topic and message type
 
-    def pc_cb(self, msg):
-        # self.data = msg.data
-        cloud = pc.read_points_list(msg, skip_nans=True)
+  def pc_cb(self, msg):
+    '''
+    Called everytime new point cloud is received
+    '''
+    points = pc.read_points_list(msg, skip_nans=True)
+    cloud = Cloud(points)
 
-        # apparently faster than converting cloud to np.array directly
-        SIZE = len(cloud)
-        x = []
-        y = []
-        z = []
-        for idx in range(SIZE):
-            if cloud[idx][2] <= 1.5:
-                x.append(cloud[idx][0])
-                y.append(cloud[idx][1])
-                z.append(cloud[idx][2])
-        self.cloud_np = np.column_stack((x, y, z))
-        self.estimate_pose()
+    planes = seq_RANSAC(cloud) # run sequential ransac
+    # random.shuffle(planes)
+    # export_planes(cloud.points, planes) 
 
-        exit()
+    # identify the plane
+    color_indices = []
+    for i in range(3):
+      rgb_array = cloud.rgb[planes[i].inlier_indices]
+      rgb_array = rgb_array.astype(np.uint8)
+      hsv_color = cv2.cvtColor(rgb_array.reshape(-1, 1, 3), cv2.COLOR_RGB2HSV)
+      hsv_squeezed = np.squeeze(hsv_color, axis=1)
 
-    def gram_schmidt(self, vectors):
-        num_vectors, vector_length = vectors.shape
-        orthogonalized_vectors = np.zeros((num_vectors, vector_length))
+      red_mask = np.logical_or(hsv_squeezed[:, 0] > 150, hsv_squeezed[:, 0] < 30)
+      green_mask = np.logical_and(hsv_squeezed[:, 0] > 45, hsv_squeezed[:, 0] < 90)
+      blue_mask = np.logical_and(hsv_squeezed[:, 0] > 90, hsv_squeezed[:, 0] < 120)
 
-        for i in range(num_vectors):
-            vector = vectors[i]
-            for j in range(i):
-                projection = np.dot(vectors[i], orthogonalized_vectors[j]) / np.dot(orthogonalized_vectors[j], orthogonalized_vectors[j])
-                vector -= projection * orthogonalized_vectors[j]
+      red_pcs = hsv_squeezed[red_mask]
+      green_pcs = hsv_squeezed[green_mask]
+      blue_pcs = hsv_squeezed[blue_mask]
 
-            orthogonalized_vectors[i] = vector / np.linalg.norm(vector)
+      color_idx = np.argmax([len(red_pcs), len(green_pcs), len(blue_pcs)])
+      color_indices.append(color_idx)
 
-        return orthogonalized_vectors
+    red_plane = planes[np.where(np.array(color_indices)==0)[0][0]]
+    green_plane = planes[np.where(np.array(color_indices)==1)[0][0]]
+    blue_plane = planes[np.where(np.array(color_indices)==2)[0][0]]
 
-    def estimate_pose(self):
-        
-        planes, inliers = self.seq_ransac(self.cloud_np, n_iters=400, threshold=0.005, n_planes=3)
+    x_vec = vec_from_planes_intersection(red_plane, blue_plane)
+    y_vec = vec_from_planes_intersection(green_plane, red_plane)
+    z_vec = vec_from_planes_intersection(green_plane, blue_plane)
+    planes_intersect_vec = np.vstack((x_vec, y_vec, z_vec))
 
-        planes_intersect_vec = []
-        for x in itertools.combinations(planes, 2):
-            normal1 = x[0][0:3]
-            normal2 = x[1][0:3]
+    # find intersection vector between planes 
+    # planes_intersect_vec = []
+    # for x in itertools.combinations(planes, 2):
+    #   dir_vec_normal = vec_from_planes_intersection(x[0], x[1])
+    #   planes_intersect_vec.append(dir_vec_normal)
 
-            dir_vec = np.cross(normal1, normal2)
-            dir_vec_normal = dir_vec / np.linalg.norm(dir_vec)
+    orthogonalized_vec = gram_schmidt(np.asarray(planes_intersect_vec)) # orthogonalize vectors
+    rotation_mat = np.transpose(orthogonalized_vec)
 
-            z = dir_vec_normal[2]
+    # find the intersection of three planes 
+    origin = np.linalg.solve(np.array([planes[i].normal for i in range(3)]), -1*np.array([planes[i].d for i in range(3)]))
+    rotation = Rotation.from_matrix(rotation_mat)
+    quaternion = rotation.as_quat()
 
-            # make sure the three vectors are facing the camera
-            if z > 0:
-                dir_vec_normal = dir_vec_normal * -1
-                
-            planes_intersect_vec.append(dir_vec_normal)
+    # publish tf for visualization
+    transform_msg = TransformStamped()
+    transform_msg.header.stamp = self.get_clock().now().to_msg()
+    transform_msg.header.frame_id = 'camera_depth_optical_frame'
+    transform_msg.child_frame_id = 'calibration_box'
+    transform_msg.transform.translation.x = origin[0]
+    transform_msg.transform.translation.y = origin[1]
+    transform_msg.transform.translation.z = origin[2]
+    transform_msg.transform.rotation.x = quaternion[0]
+    transform_msg.transform.rotation.y = quaternion[1]
+    transform_msg.transform.rotation.z = quaternion[2]
+    transform_msg.transform.rotation.w = quaternion[3]
 
-        # calculating rotation matrix 
-        planes_intersect_vec = np.array(planes_intersect_vec)
-        orthogonalized_vec = self.gram_schmidt(planes_intersect_vec)
-        rotation_mat = np.transpose(orthogonalized_vec)
+    tf_msg = TFMessage()
+    tf_msg.transforms = [transform_msg]
+    self.publisher_.publish(tf_msg)
 
-        # intersection of three planes yields a point
-        planes = np.array(planes)
-        origin = np.linalg.solve(planes[:, 0:3], -1*planes[:, 3])
+    # print for debugging 
+    print("vectors ", planes_intersect_vec)
+    print("orthogonalized ", orthogonalized_vec)
+    print("rotation mat ", rotation_mat)
+    print("origin ", origin)
+    print("quaternion ", quaternion)
 
-        # convert to quaternion
-        rotation = Rotation.from_matrix(rotation_mat)
-        quaternion = rotation.as_quat()
+    exit() # run once
 
-        print("vectors ", planes_intersect_vec)
-        print("orthogonalized ", orthogonalized_vec)
-        print("rotation mat ", rotation_mat)
-        print("origin ", origin)
-        print("quaternion ", quaternion)
+def estimate_pose(self):
+  planes, inliers = self.seq_ransac(self.cloud_np, n_iters=400, threshold=0.005, n_planes=3)
 
-    def seq_ransac(self, points, n_iters=1000, threshold=0.005, n_planes=3):
-        n_points = len(points)
+  color_indices = []
+  for i in range(3):
 
-        points_indices = np.arange(n_points)
-        remaining_points = points
-            
-        final_planes = []
-        final_inliers = []
-        for i in range(n_planes):
+    rgb_array = self.rgb[inliers[i]]
+    rgb_array = rgb_array.astype(np.uint8)
+    hsv_color = cv2.cvtColor(rgb_array.reshape(-1, 1, 3), cv2.COLOR_RGB2HSV)
+    hsv_squeezed = np.squeeze(hsv_color, axis=1)
 
-            p = 0.99
-            s = 3
-            num_inliers = 45000
-            eps = (len(remaining_points) - num_inliers) / len(remaining_points)
-            N = round(np.log(1-p) / np.log(1-(1-eps)**s))
-            print(N)
+    red_mask = np.logical_or(hsv_squeezed[:, 0] > 150, hsv_squeezed[:, 0] < 30)
+    green_mask = np.logical_and(hsv_squeezed[:, 0] > 45, hsv_squeezed[:, 0] < 90)
+    blue_mask = np.logical_and(hsv_squeezed[:, 0] > 90, hsv_squeezed[:, 0] < 120)
 
-            # plane, inliers = self.ransac_plane_fit(points=remaining_points, n_iters=n_iters, threshold=threshold)
-            plane, inliers = self.ransac_plane_fit(points=remaining_points, n_iters=N, threshold=threshold)
+    red_pcs = hsv_squeezed[red_mask]
+    green_pcs = hsv_squeezed[green_mask]
+    blue_pcs = hsv_squeezed[blue_mask]
 
-            final_planes.append(plane)
-            final_inliers.append(points_indices[inliers])
+    color_idx = np.argmax([len(red_pcs), len(green_pcs), len(blue_pcs)])
+    color_indices.append(color_idx)
 
-            # remove the inliers
-            mask = np.ones(len(remaining_points), dtype=bool)
-            mask[inliers] = False
-            remaining_points = remaining_points[mask]
+  red_plane = planes[np.where(np.array(color_indices)==0)[0][0]]
+  green_plane = planes[np.where(np.array(color_indices)==1)[0][0]]
+  blue_plane = planes[np.where(np.array(color_indices)==2)[0][0]]
 
-            # remove the original points indices belong to inliers
-            mask2 = np.ones(len(points_indices), dtype=bool)
-            mask2[inliers] = False
-            points_indices = points_indices[mask2]
+  x_vec = self.vec_from_planes_intersection(red_plane, green_plane)
+  y_vec = self.vec_from_planes_intersection(blue_plane, red_plane)
+  z_vec = self.vec_from_planes_intersection(green_plane, blue_plane)
 
-        return final_planes, final_inliers
+  planes_intersect_vec = np.vstack((x_vec, y_vec, z_vec))
 
-    def ransac_plane_fit(self, points, n_iters=100, threshold=8):
+  print("planes_intersect_vec: ", planes_intersect_vec)
 
-        best_plane_coeffs = None
-        best_inliers = None
-        max_inliers = 0
+  # calculating rotation matrix 
+  # planes_intersect_vec = np.array(planes_intersect_vec)
+  orthogonalized_vec = self.gram_schmidt(planes_intersect_vec)
+  rotation_mat = np.transpose(orthogonalized_vec)
 
-        for i in range(n_iters):
-            # Randomly select three points to define a plane
-            random_indices = np.random.choice(len(points), 3, replace=False)
-            sample_points = points[random_indices]
+  print("orthogonalized_vec", orthogonalized_vec)
 
-            # Compute the normal vector of the plane using the selected points
-            v1 = sample_points[1] - sample_points[0]
-            v2 = sample_points[2] - sample_points[0]
-            normal = np.cross(v1, v2)
-            normal /= np.linalg.norm(normal)
+  # intersection of three planes yields a point
+  planes = np.array(planes)
+  origin = np.linalg.solve(planes[:, 0:3], -1*planes[:, 3])
 
-            # Calculate the distance from each point to the plane
-            distances = np.abs(np.dot(points - sample_points[0], normal))
+  # convert to quaternion
+  rotation = Rotation.from_matrix(rotation_mat)
+  quaternion = rotation.as_quat()
 
-            # Count the inliers (points close enough to the plane)
-            # inliers = points[distances < threshold]
-            inliers = np.where(distances < threshold)[0]
-            num_inliers = len(inliers)
-
-            # Update the best plane if necessary
-            if num_inliers > max_inliers:
-                # Calculate plane coefficients [a, b, c, d]
-                d = -np.dot(normal, sample_points[0])
-                plane_coeffs = np.append(normal, d)
-
-                best_plane_coeffs = plane_coeffs
-                best_inliers = inliers
-                max_inliers = num_inliers
-
-        return best_plane_coeffs, best_inliers
+  print("vectors ", planes_intersect_vec)
+  print("orthogonalized ", orthogonalized_vec)
+  print("rotation mat ", rotation_mat)
+  print("origin ", origin)
+  print("quaternion ", quaternion)
 
 def main(args=None):
-    rclpy.init(args=args)
-    node = MyNode()
-    rclpy.spin(node)
-
+  rclpy.init(args=args)
+  node = MyNode()
+  rclpy.spin(node)
