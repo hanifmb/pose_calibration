dc057f457cc4283e43f95b017297ce194fdef255
hanifmb <hanifdiyer@gmail.com>
2023-08-23 04:20:17 +0200

    add more frames, publish tf with timer

--- pose_calibration/pose_calibration_node.py
+++ pose_calibration/pose_calibration_node.py
@@ -63,6 +63,129 @@ class Cloud():
     rgb = np.column_stack((r, g, b))
     return points, rgb
 
+class MyNode(Node):
+  def __init__(self):
+    super().__init__('my_node')
+    self.pc_sub = self.create_subscription(PointCloud2, '/camera/depth/color/points', self.pc_cb, 10)
+    self.calib_srv = self.create_service(Empty, '/pose_calibration/get_pose', self.calib_cb)
+    self.tf_pub = self.create_publisher(TFMessage, '/tf', 10)  
+    self.timer = self.create_timer(0.1, self.publish_transform)
+    self.points = []
+    self.tf_msg = None
+
+  def publish_transform(self):
+    if self.tf_msg is None: return
+    self.tf_pub.publish(self.tf_msg)
+
+  def pc_cb(self, msg):
+    self.points = pc.read_points_list(msg, skip_nans=True)
+
+  def calib_cb(self, request, response):
+
+    if self.points == []: print("Point cloud is empty!"); return response
+    cloud = Cloud(self.points)
+    planes = seq_ransac(cloud) # run sequential ransac
+    # export_planes(cloud.points, planes) 
+
+    # identify the plane
+    # need refactor
+    color_indices = []
+    for i in range(3):
+      rgb = cloud.rgb[planes[i].inlier_indices]
+      rgb = rgb.astype(np.uint8)
+      hsv = cv2.cvtColor(rgb.reshape(-1, 1, 3), cv2.COLOR_RGB2HSV)
+      hsv_squeezed = np.squeeze(hsv, axis=1)
+
+      red_mask = np.logical_or(hsv_squeezed[:, 0] > 170, hsv_squeezed[:, 0] < 10)
+      green_mask = np.logical_and(hsv_squeezed[:, 0] > 45, hsv_squeezed[:, 0] < 75)
+      blue_mask = np.logical_and(hsv_squeezed[:, 0] > 90, hsv_squeezed[:, 0] < 110)
+
+      red_pcs = hsv_squeezed[red_mask]
+      green_pcs = hsv_squeezed[green_mask]
+      blue_pcs = hsv_squeezed[blue_mask]
+
+      print(f"r:{len(red_pcs)} g:{len(green_pcs)} b:{len(blue_pcs)}")
+
+      color_idx = np.argmax([len(red_pcs), len(green_pcs), len(blue_pcs)])
+      color_indices.append(color_idx)
+
+    print(f"indices: {color_indices}")
+    red_plane = planes[np.where(np.array(color_indices)==0)[0][0]]
+    green_plane = planes[np.where(np.array(color_indices)==1)[0][0]]
+    blue_plane = planes[np.where(np.array(color_indices)==2)[0][0]]
+
+    x_vec = calc_vec(red_plane, blue_plane)
+    y_vec = calc_vec(green_plane, red_plane)
+    z_vec = calc_vec(green_plane, blue_plane)
+    planes_intersect_vec = np.vstack((x_vec, y_vec, z_vec))
+
+    orthogonalized_vec = gram_schmidt(np.asarray(planes_intersect_vec)) # orthogonalize vectors
+    rotation_mat = np.transpose(orthogonalized_vec)
+    quaternion = Rotation.from_matrix(rotation_mat).as_quat()
+
+    # find three planes intersection
+    origin = np.linalg.solve(np.array([planes[i].normal for i in range(3)]), -1*np.array([planes[i].d for i in range(3)]))
+
+    # find t_ct
+    # transformation of world w.r.t camera
+    t_cw = np.eye(4)
+    t_cw [:3, :3] = rotation_mat 
+    t_cw [:3, 3] = origin 
+
+    # transformation of robot's base w.r.t world
+    t_rw = np.array([[-1.0000000,  0.0000000,  0.0000000, 0.314],
+                     [0.0000000,  -1.0000000,  0.0000000, 0.947],
+                     [0.0000000,  0.0000000,  1.0000000, 0.029],
+                     [0.0000000, 0.0000000, 0.0000000, 1.0000000]])
+    t_wr = np.linalg.inv(t_rw)
+
+    # transformation of arm w.r.t robot's base
+    t_rt = np.array([[0.3520344, -0.7203782, -0.5976012, -0.174470],
+                     [0.5511497,  0.6755850, -0.4897130, 0.560472],
+                     [0.7565089, -0.1569719,  0.6348653, 0.680237],
+                     [0.0000000, 0.0000000, 0.0000000, 1.0000000]])
+
+    t_cr = t_cw @ t_wr
+    t_ct = t_cr @ t_rt
+    # print(f"t_cw: {t_cw}\n t_rw: {t_wr}\n t_rt: {t_rt}\n")
+    print("t_ct: ", t_wr)
+
+    tcw_msg = create_transform(self, t_cw, 'camera_depth_optical_frame', 'calibration_box')
+    tcr_msg = create_transform(self, t_cr, 'camera_depth_optical_frame', 'robot_base')
+    tct_msg = create_transform(self, t_ct, 'camera_depth_optical_frame', 'tool')
+
+    self.tf_msg = TFMessage()
+    # tf_msg.transforms = [tcw_msg, tcr_msg, tct_msg]
+    self.tf_msg.transforms = [tcw_msg]
+    # self.tf_pub.publish(tf_msg)
+
+    # print for debugging 
+    print("vectors ", planes_intersect_vec)
+    print("orthogonalized ", orthogonalized_vec)
+    print("rotation mat ", rotation_mat)
+    print("origin ", origin)
+    print("quaternion ", quaternion)
+    return response
+
+def create_transform(node, t, parent, child):
+
+  r = t[:3, :3]
+  quaternion = Rotation.from_matrix(r).as_quat()
+  translation = t[:3, 3]
+  transform_msg = TransformStamped()
+  transform_msg.header.stamp = node.get_clock().now().to_msg()
+  transform_msg.header.frame_id = parent
+  transform_msg.child_frame_id = child
+  transform_msg.transform.translation.x = translation[0]
+  transform_msg.transform.translation.y = translation[1]
+  transform_msg.transform.translation.z = translation[2]
+  transform_msg.transform.rotation.x = quaternion[0]
+  transform_msg.transform.rotation.y = quaternion[1]
+  transform_msg.transform.rotation.z = quaternion[2]
+  transform_msg.transform.rotation.w = quaternion[3]
+
+  return transform_msg
+
 def seq_ransac(cloud, threshold=0.005, n_planes=3):
   def ransac_plane_fit(points, n_iters):
     best_plane = []
@@ -149,88 +272,6 @@ def export_planes(points, planes):
                                        formats = 'f4, f4, f4, u1, u1, u1')
   el = PlyElement.describe(new_ver, 'vertex')
   PlyData([el], text=True).write('./ascii.ply')
-
-class MyNode(Node):
-  def __init__(self):
-    super().__init__('my_node')
-    self.pc_sub = self.create_subscription(PointCloud2, '/camera/depth/color/points', self.pc_cb, 10)
-    self.calib_srv = self.create_service(Empty, '/pose_calibration/get_pose', self.calib_cb)
-    self.tf_pub = self.create_publisher(TFMessage, '/tf', 10)  
-    self.points = []
-
-  def pc_cb(self, msg):
-    self.points = pc.read_points_list(msg, skip_nans=True)
-
-  def calib_cb(self, request, response):
-
-    if self.points == []: print("Point cloud is empty!"); return response
-    cloud = Cloud(self.points)
-    planes = seq_ransac(cloud) # run sequential ransac
-    # export_planes(cloud.points, planes) 
-
-    # identify the plane
-    # need refactor
-    color_indices = []
-    for i in range(3):
-      rgb = cloud.rgb[planes[i].inlier_indices]
-      rgb = rgb.astype(np.uint8)
-      hsv = cv2.cvtColor(rgb.reshape(-1, 1, 3), cv2.COLOR_RGB2HSV)
-      hsv_squeezed = np.squeeze(hsv, axis=1)
-
-      red_mask = np.logical_or(hsv_squeezed[:, 0] > 170, hsv_squeezed[:, 0] < 10)
-      green_mask = np.logical_and(hsv_squeezed[:, 0] > 45, hsv_squeezed[:, 0] < 75)
-      blue_mask = np.logical_and(hsv_squeezed[:, 0] > 90, hsv_squeezed[:, 0] < 110)
-
-      red_pcs = hsv_squeezed[red_mask]
-      green_pcs = hsv_squeezed[green_mask]
-      blue_pcs = hsv_squeezed[blue_mask]
-
-      print(f"r:{len(red_pcs)} g:{len(green_pcs)} b:{len(blue_pcs)}")
-
-      color_idx = np.argmax([len(red_pcs), len(green_pcs), len(blue_pcs)])
-      color_indices.append(color_idx)
-
-    print(f"indices: {color_indices}")
-    red_plane = planes[np.where(np.array(color_indices)==0)[0][0]]
-    green_plane = planes[np.where(np.array(color_indices)==1)[0][0]]
-    blue_plane = planes[np.where(np.array(color_indices)==2)[0][0]]
-
-    x_vec = calc_vec(red_plane, blue_plane)
-    y_vec = calc_vec(green_plane, red_plane)
-    z_vec = calc_vec(green_plane, blue_plane)
-    planes_intersect_vec = np.vstack((x_vec, y_vec, z_vec))
-
-    orthogonalized_vec = gram_schmidt(np.asarray(planes_intersect_vec)) # orthogonalize vectors
-    rotation_mat = np.transpose(orthogonalized_vec)
-    quaternion = Rotation.from_matrix(rotation_mat).as_quat()
-
-    #find three planes intersection
-    origin = np.linalg.solve(np.array([planes[i].normal for i in range(3)]), -1*np.array([planes[i].d for i in range(3)]))
-
-    # publish tf for visualization
-    transform_msg = TransformStamped()
-    transform_msg.header.stamp = self.get_clock().now().to_msg()
-    transform_msg.header.frame_id = 'camera_depth_optical_frame'
-    transform_msg.child_frame_id = 'calibration_box'
-    transform_msg.transform.translation.x = origin[0]
-    transform_msg.transform.translation.y = origin[1]
-    transform_msg.transform.translation.z = origin[2]
-    transform_msg.transform.rotation.x = quaternion[0]
-    transform_msg.transform.rotation.y = quaternion[1]
-    transform_msg.transform.rotation.z = quaternion[2]
-    transform_msg.transform.rotation.w = quaternion[3]
-
-    tf_msg = TFMessage()
-    tf_msg.transforms = [transform_msg]
-    self.tf_pub.publish(tf_msg)
-
-    # print for debugging 
-    print("vectors ", planes_intersect_vec)
-    print("orthogonalized ", orthogonalized_vec)
-    print("rotation mat ", rotation_mat)
-    print("origin ", origin)
-    print("quaternion ", quaternion)
-    return response
 
 def main(args=None):
   rclpy.init(args=args)
